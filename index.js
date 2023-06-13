b91606c1f149b81ae5c4e454c68f36f0f80ff15a
g-susvs <jesus.savs19@gmail.com>
2023-06-13 18:45:28 -0500

    Base del servidor

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
+const Server = require("./models/server");
+
 require("dotenv").config();
-const express = require("express");
 
-const app = express();
-const port = process.env.PORT;
-
-app.get("/", (req, res) => {
-  res.send("ZONA DE GOL - API");
-});
-
-app.listen(port, () => {
-  console.log(`http://localhost:${port}`);
-});
+const server = new Server();
+server.listen();
